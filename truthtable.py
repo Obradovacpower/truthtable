38f07d85dff9a526515ef5d2d251163f0688be63
Esben Bjerre <hej@esbenbjerre.dk>
2018-02-08 20:10:45 +0100

    Changed encoding

--- truthtable.py
+++ truthtable.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2.7
-# coding: latin-1
+# coding: utf-8
 
 table = """+-------+-------+--------+-------+-------+-------+-------+
 |   P   |   Q   |   ¬P   | P ∧ Q | P ∨ Q | P ⇒ Q | P ⇔ Q |
